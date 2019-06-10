e3f0b31e18d37d7082a504291f0077099deea39a
Jason Wu <rod5438@gmail.com>
2019-06-10 17:12:22 +0800

    Try custom css for form.

--- author_diary_form.php
+++ author_diary_form.php
@@ -2,34 +2,49 @@
 /*
  * Template Name: Author diary form
  */
-
 ?>
-
-<style type="text/css">
-</style>
 
 <?php
 	function getForm () {
 		$form_html_string = '<div style="margin: 60px">
 								<form action="" target="_self" method="post">
 								標題:<br>
-								<input type="text" name="title"><br>
-								日記:<br>
-								<textarea rows="20" name="diary"></textarea><br>
+								<input type="text" name="title" value="我的日記" required><br>
+								日記第一段:<br>
+								<textarea rows="20" name="diary1" required>日記第一段</textarea><br>
+								日記第二段:<br>
+								<textarea rows="20" name="diary2"></textarea><br>
 								<input type="submit" value="送出班長日記"><br>
 								</form>
 							</div>';
 		echo $form_html_string;
 	}
 	function canShowForm () {
-		return empty($_POST['title']) || empty($_POST['diary']);
+		return empty($_POST['title']) || empty($_POST['diary1'] || empty($_POST['diary2']));
+	}
+	function createContent () {
+		$titleStyle = 'styleTitle';
+		$diary1Style = 'style1';
+		$diary2Style = 'style2';
+
+		$title = $_POST['title'];
+		$diary1 = $_POST['diary1'];
+		$diary2 = $_POST['diary2'];
+
+		$content = '<div>
+						<p class="'.$titleStyle.'">'.$title.'</p>
+						<p class="'.$diary1Style.'">'.$diary1.'</p>
+						<p class="'.$diary2Style.'">'.$diary2.'</p>
+					</div>';
+		return $content;
 	}
 	function createPost () {
+		$content = createContent();
 		$post_id = wp_insert_post(
 			array(
 				'post_title'  => $_POST['title'],
 				'post_type'   => 'post',
-				'post_content' => $_POST['diary'],
+				'post_content' => $content,
 				'post_status' => 'publish',
 			)
 		);
